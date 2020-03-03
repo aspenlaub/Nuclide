dab71d5e3ed126d4ee5611b6acc527eefe0234d7
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2020-03-03 21:47:33 +0100

    Package-to-push class and interface must also be moved

--- src/Interfaces/IPackageToPush.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Aspenlaub.Net.GitHub.CSharp.Nuclide.Interfaces {
-    public interface IPackageToPush {
-        string PackageFileFullName { get; set; }
-        string FeedUrl { get; set; }
-        string ApiKey { get; set; }
-        string Id { get; set; }
-        string Version { get; set; }
-    }
-}
