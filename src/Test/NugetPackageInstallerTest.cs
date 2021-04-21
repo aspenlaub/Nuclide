78c32058a086fc6e8928585f221e0bead6cbc1e3
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-04-21 20:32:30 +0200

    Cake tools folder has been deleted

--- src/Test/NugetPackageInstallerTest.cs
+++ src/Test/NugetPackageInstallerTest.cs
@@ -50,19 +50,6 @@ namespace Aspenlaub.Net.GitHub.CSharp.Nuclide.Test {
             Assert.IsFalse(errorsAndInfos.Errors.Any(), errorsAndInfos.ErrorsPlusRelevantInfos());
             Assert.IsTrue(errorsAndInfos.Infos.Any(i => i.Contains("Adding package") && i.Contains("to folder")));
             Assert.IsTrue(ChabStandardTarget.Folder().SubFolder(@"src\OctoPack.3.6.0").Exists());
-
-            errorsAndInfos = new ErrorsAndInfos();
-            const string oldCakeVersion = "0.24.0";
-            sut.InstallNugetPackage(ChabStandardTarget.Folder().SubFolder("tools"), "Cake", oldCakeVersion, true, errorsAndInfos);
-            Assert.IsFalse(errorsAndInfos.Errors.Any());
-            Assert.IsTrue(ChabStandardTarget.Folder().SubFolder(@"tools\Cake").Exists());
-            Assert.IsTrue(File.Exists(ChabStandardTarget.Folder().SubFolder(@"tools\Cake").FullName + @"\Cake.exe"));
-
-            errorsAndInfos = new ErrorsAndInfos();
-            sut.InstallNugetPackage(ChabStandardTarget.Folder().SubFolder("tools"), "Cake", "", true, errorsAndInfos);
-            Assert.IsFalse(errorsAndInfos.Errors.Any());
-            Assert.IsTrue(errorsAndInfos.Infos.Any(i => i.Contains("Successfully uninstalled") && i.Contains(oldCakeVersion)));
-            Assert.IsTrue(errorsAndInfos.Infos.Any(i => i.Contains("Successfully installed")));
         }
     }
 }
